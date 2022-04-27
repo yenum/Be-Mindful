c78f6b7131933a80fabc3442ada202e99dac1eba
Yenum <87123146+yenum@users.noreply.github.com>
2022-04-27 13:00:32 +0100

    minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css';
 import Homepage from './pages/HomePage/Homepage';
+import Signup from './pages/Signup/Signup';
+import Signin from './pages/Signin/Signin';
 
 function App() {
   return (
-  <Homepage />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Homepage />}/>
+      <Route path="/signup" element={<Signup/>}/>
+      <Route path='/signin' element={<Signin/>}/>
+    </Routes>
+    </BrowserRouter>
+  
   );
 }
 
